a23f0eddde66ccb73a64509dfc124da914115fa3
HYH\a98k9 <a98k98k@gmail.com>
2022-03-12 19:56:00 +0900

    Mosaic Web Project vue.js Spring Boot 연동

---
+++ web/src/main/java/com/codingpotato/controller/WebController.java
@@ -0,0 +1,16 @@
+package com.codingpotato.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class WebController {
+
+    @GetMapping("/vue")
+    public String vue(){
+
+        return "vue/index";
+    }
+}
