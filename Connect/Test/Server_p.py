aef100b1a71d06e3b373fdc7702dd6c11213221e
SeungWook0502 <SeungWook0502@gmail.com>
2022-04-06 20:22:19 +0900

    Connect
    Python Server-Client Test

---
+++ Connect/Test/Server_p.py
@@ -0,0 +1,54 @@
+import socket
+import struct
+import pickle
+import cv2
+
+ip = socket.gethostbyname(socket.gethostname())
+port = 50001
+
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+server_socket.bind((ip, port))
+
+server_socket.listen(10) 
+
+
+client_socket, address = server_socket.accept()
+print('connect ip -', address[0])
+
+data_buffer = b""
+
+data_size = struct.calcsize("L")
+
+while True:
+    
+    while len(data_buffer) < data_size:
+        data_buffer += client_socket.recv(4096)
+
+    packed_data_size = data_buffer[:data_size]
+    data_buffer = data_buffer[data_size:] 
+    
+    frame_size = struct.unpack(">L", packed_data_size)[0]
+    
+
+    while len(data_buffer) < frame_size:
+        data_buffer += client_socket.recv(4096)
+    
+    frame_data = data_buffer[:frame_size]
+    data_buffer = data_buffer[frame_size:]
+    print("Frame size - {} bytes".format(frame_size))
+    
+    frame = pickle.loads(frame_data)
+    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+    
+    cv2.imshow('Frame', frame)
+    
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+
+client_socket.close()
+server_socket.close()
+print('close...')
+
+cv2.destroyAllWindows()
