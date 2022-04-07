0fc32edf062e3f30ec1d6dad1374cb2f9f64a5ca
SeungWook0502 <SeungWook0502@gmail.com>
2022-04-07 23:21:20 +0900

    Connect
    Python Multi Server - Java Client Test

--- Connect/Test/Java_Client/src/Java_Client_Test/Client_test1.java
+++ Connect/Test/Java_Client/src/Java_Client_Test/Client_test1.java
@@ -1,38 +1,117 @@
 package Java_Client_Test;
 
 import java.net.*;
+import java.nio.ByteBuffer;
+import java.awt.Image;
 import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
+
+import javax.imageio.ImageIO;
+
+import org.opencv.core.*;
+import org.opencv.videoio.*;
+import org.opencv.imgcodecs.Imgcodecs;
 
 public class Client_test1 {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		
+
+//		System.loadLibrary(Core.NATIVE_LIBRARY_NAME); // necessary part of running OpenCV
+
+		String ip = "192.168.0.149";
 		int port = 50001;
-		DataOutputStream dos;
-		OutputStream os;
+		String file_path = "D:\\School\\FinalProject\\VideoMosaicWeb\\Connect\\Data\\pns_color.jpg";
+		byte[] bytes = new byte[4096];
+		Image image;
 		
-		try(Socket socket = new Socket("192.168.0.149", port)){
+//		VideoCapture vc = new VideoCapture(file_path);
 			
-//			dos = new DataOutputStream(socket.getOutputStream());
-			os = socket.getOutputStream();
+			DataOutputStream dos;
+			OutputStream sock_os, file_os;
 			
-			while(true) {
+			InputStream is;
+			ByteBuffer bb;
+			
+			try(Socket socket = new Socket("192.168.0.149", port)){
+
 				
-				Scanner scanner = new Scanner(System.in);
-				String command = scanner.nextLine(); //Enter command
 				
-				os.write(command.getBytes("UTF-8"));
+				sock_os = socket.getOutputStream();
 				
-//				dos.writeUTF(command);
+				is = new BufferedInputStream(new FileInputStream(file_path));
+				
+				sock_os.write(is.readAllBytes());
+						
+//				while(true) {
+					
+					
+					
+//					Mat frame = new Mat();
+//					MatOfByte bytemat = new MatOfByte();
+//					
+//					vc.grab();
+//					vc.retrieve(frame);
+//
+//					double[] vec = frame.get(0,0);
+//					
+//					System.out.println(frame.get(frame_wh, bytes));
+//					System.out.println(frame_wh[0]);
+//					System.out.println(frame_wh[1]);
+//					Rect rect = new Rect(0, 0, mat.rows(), mat.cols());
+					
+					
+//					Rect[] mat_reval = ((MatOfByte) mat).toArray();
+					
+//					Imgcodecs.imencode(".jpg", mat, bytemat);
+//					
+//					System.out.println(mat.dump());
+//				}
+				
+			} catch (UnknownHostException e) {
+				System.out.println("Connect lost Exception");
+			} catch (IOException e) {
+				System.out.println("IO Exception");
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			
-		} catch (UnknownHostException e) {
-			System.out.println("Connect lost Exception");
-		} catch (IOException e) {
-			System.out.println("IO Exception");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		
 	}
+		
 }
+//public class Client_test1 {
+//	
+//	public static void main(String[] args){
+//		
+//		String ip = "192.168.0.149";
+//		int port = 50001;
+//		String file_path = "D:\\School\\FinalProject\\VideoMosaicWeb\\Connect\\Data\\slice_test2.mp4";
+//		byte[] bytes = new byte[4096];
+//		
+//		DataOutputStream dos;
+//		OutputStream os;
+//		InputStream is;
+//		
+//		try(Socket socket = new Socket(ip,  port)){
+//			
+//			is = new FileInputStream(new File(file_path));
+//			os = socket.getOutputStream();
+//			
+//			int count = is.read(bytes, 0, 4096);
+//			
+//			while(count != -1) {
+//				
+//				os.write(bytes, 0, 4096);
+//				count = is.read(bytes, 0, 4096);
+//			}
+//			
+//		} catch (UnknownHostException e) {
+//			System.out.println("Connect lost Exception");
+//		} catch (IOException e) {
+//			System.out.println("IO Exception");
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//}
--- Connect/Test/Server_p.py
+++ Connect/Test/Server_p.py
@@ -8,66 +8,39 @@ port = 50001
 
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-# 소켓 주소 정보 할당
 server_socket.bind((ip, port))
-
-# 연결 리스닝(동시 접속) 수 설정
 server_socket.listen(10) 
 
 
-# 연결 수락(클라이언트 (소켓, 주소 정보) 반환)
 client_socket, address = server_socket.accept()
 print('connect ip -', address[0])
 
-# 수신한 데이터를 넣을 버퍼(바이트 객체)
 data_buffer = b""
 
-# calcsize : 데이터의 크기(byte)
-# - L : 부호없는 긴 정수(unsigned long) 4 bytes
 data_size = struct.calcsize("L")
 
 while True:
-    # 설정한 데이터의 크기보다 버퍼에 저장된 데이터의 크기가 작은 경우
     while len(data_buffer) < data_size:
-        # 데이터 수신
         data_buffer += client_socket.recv(4096)
 
-    # 버퍼의 저장된 데이터 분할
     packed_data_size = data_buffer[:data_size]
-    data_buffer = data_buffer[data_size:] 
-    
-    # struct.unpack : 변환된 바이트 객체를 원래의 데이터로 변환
-    # - > : 빅 엔디안(big endian)
-    #   - 엔디안(endian) : 컴퓨터의 메모리와 같은 1차원의 공간에 여러 개의 연속된 대상을 배열하는 방법
-    #   - 빅 엔디안(big endian) : 최상위 바이트부터 차례대로 저장
-    # - L : 부호없는 긴 정수(unsigned long) 4 bytes 
+    data_buffer = data_buffer[data_size:]
     frame_size = struct.unpack(">L", packed_data_size)[0]
     
-    # 프레임 데이터의 크기보다 버퍼에 저장된 데이터의 크기가 작은 경우
     while len(data_buffer) < frame_size:
-        # 데이터 수신
         data_buffer += client_socket.recv(4096)
     
-    # 프레임 데이터 분할
     frame_data = data_buffer[:frame_size]
     data_buffer = data_buffer[frame_size:]
     
     print("Frame size - {} bytes".format(frame_size))
     
-    # loads : 직렬화된 데이터를 역직렬화
-    # - 역직렬화(de-serialization) : 직렬화된 파일이나 바이트 객체를 원래의 데이터로 복원하는 것
     frame = pickle.loads(frame_data)
-    
-    # imdecode : 이미지(프레임) 디코딩
-    # 1) 인코딩된 이미지 배열
-    # 2) 이미지 파일을 읽을 때의 옵션
-    #    - IMREAD_COLOR : 이미지를 COLOR로 읽음
     frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
     
-    # 프레임 출력
+    print(frame[:10])
     cv2.imshow('Frame', frame)
     
-    # 'q' 키를 입력하면 종료
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
         break
