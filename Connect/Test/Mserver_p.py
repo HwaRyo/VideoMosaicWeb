567cfa501cc83f7ca74ffb25dba1f96c8437eda0
SeungWook0502 <SeungWook0502@gmail.com>
2022-04-07 23:23:24 +0900

    Connect
    Python Multi Server - Java Client Test

--- Connect/Test/Mserver_p.py
+++ Connect/Test/Mserver_p.py
@@ -2,7 +2,6 @@ import socket
 from threading import Thread
 
 def echo_handler(conn, addr, terminator="bye"):
-    '''개별 연결에 대해 에코잉을 처리하는 핸들러'''
     BUF_SIZE = 1024
     while True:
         data = conn.recv(BUF_SIZE)
@@ -19,7 +18,6 @@ def run_server(host = '192.168.0.149', port = 50001):
         while True:
             sock.listen(3)
             conn, addr = sock.accept()
-            # 새 연결이 생성되면 새 스레드에서 에코잉을 처리하게 한다.
             t = Thread(target=echo_handler, args=(conn, addr))
             t.start()
         sock.close()
