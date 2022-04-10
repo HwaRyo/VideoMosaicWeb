c44349024f3f3bf769bd408ab851c89ba00e2c32
leonilpark <pjangh98@naver.com>
2022-04-10 15:51:55 +0900

    run.py : module_face_recognition parameter edit

--- face_detector/run.py
+++ face_detector/run.py
@@ -11,7 +11,7 @@ video_dir = ""
 
 sample_gathering = module_sample_gathering(face_dir, name, id)
 face_detection = module_face_detection(dataset_dir)
-face_recognition = module_face_recognition(model_dir)
+face_recognition = module_face_recognition(model_dir,video_dir)
 
 sample_gathering.video_capture()
 face_detection.process()
