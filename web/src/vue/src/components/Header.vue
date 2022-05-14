87ef69c7fddb7d90ac8c397eceab7dd3f118e7b2
pjsu226 <pjsu226@naver.com>
2022-05-14 16:33:24 +0900

    Mosaic Web Project

---
+++ web/src/vue/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="hold">
+  <div class="header">
+    <div class="container">
+      <div id="logo">
+      </div>
+      <ul class="nav">
+        <li><a href="/login">로그인</a></li>
+        <li><a href="/Announcement">공지사항</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+
+</style>
