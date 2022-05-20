4dafc2b4f624506eaa34dad22fb8a053a1aa88a2
HYH\a98k9 <a98k98k@gmail.com>
2022-05-20 15:27:08 +0900

    Mosaic Web Project 게시판(진행중)

--- vue/src/components/FileDownload.vue
+++ vue/src/components/FileDownload.vue
@@ -2,11 +2,37 @@
 <div class="wrap">
     <button v-on:click="insert()">등록</button>
     <button v-on:click="index()">목록</button>
+    <table>
+        <thead>
+            <tr>
+                <th>No.</th>
+                <th>게시글</th>
+                <th>콘텐츠</th>
+                <th>업로드날짜</th>
+                <th>수정날짜</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr class="content" @click="move()" v-for="(v, i) in datas" :key="i">
+                <td>{{i+1}}</td>
+                <td>{{v.title}}</td>
+                <td>{{v.content}}</td>
+                <td>{{v.createDate}}</td>
+                <td>{{v.modifiedDate}}</td>
+            </tr>
+        </tbody>
+    </table>
 </div>
 </template>
 <script>
 export default {
     name: 'VideoDownload',
+    data(){
+        return{
+            datas: [],
+        }
+    },
     methods:{
          insert(){
             const Token = localStorage.getItem('token');
@@ -34,6 +60,7 @@ export default {
             const headers = {
                 'Authorization': 'Bearer '+Token
             }
+            console.log(Token)
             const url = 'http://localhost:8080/indexboard';
             this.axios({
                 method: 'get',
@@ -41,10 +68,14 @@ export default {
                 headers: headers,
             })
             .then(({data}) => {
+                this.datas = data
                 console.log('axios get success', data);
             }).catch((err) =>{
                 console.log('err',err);
             })
+        },
+        move(){
+            location.href = "/";
         }
     },
 };
