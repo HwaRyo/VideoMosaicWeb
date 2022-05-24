207513fa9e46043ca9804574c9cc9be28c40e11a
SeungWook0502 <SeungWook0502@gmail.com>
2022-05-24 13:36:59 +0900

    Object tracking
    module connect

---
+++ ObjectTracking/mailsend.py
@@ -0,0 +1,28 @@
+import smtplib
+from email.mime.text import MIMEText
+
+class mailSend:
+    def __init__(self, userEmail):
+        self.smtpServer =  "smtp.naver.com"
+        self.projectEmail = "wjstmdnr@naver.com"
+        f = open('pw.txt', 'r')
+        self.projectPw = f.readline()
+        self.smtpPort = 587
+        self.userEmail = userEmail
+
+    def send(self, title, content):
+            
+        message = MIMEText(content,_charset="utf8")
+
+        message['Subject'] = title 
+        message['From'] = self.projectEmail
+        message['To'] = self.userEmail
+                
+        smtp = smtplib.SMTP(self.smtpServer, self.smtpPort)
+        smtp.ehlo
+        smtp.starttls()
+        smtp.login(self.projectEmail , self.projectPw)
+                
+        smtp.sendmail(message['From'], message['To'].split(','), message.as_string())
+
+        smtp.quit()
